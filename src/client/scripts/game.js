18039f4f60314ece4dcd89d9b50b139c2c8e504e
Yuval Fuks <yuvalfuks@hotmail.com>
2020-03-21 19:41:27 +0200

    server side done (?)

--- src/client/scripts/game.js
+++ src/client/scripts/game.js
@@ -4,10 +4,19 @@ $().ready(() => {
         el: '#app',
         data: {
             Game : {},
+            myUser : {},
             myNickname : window.localStorage.getItem('nickname')
         },
         methods: {
-            
+            refresh : async () => {
+                setInterval(async () => {
+                    this.Game = await $.getJSON('/users')
+                    myUser = this.Game.users.find(user => user.nickname == this.myNickname)
+                    console.log(this.Game)
+                }, 200)
+            }
         }
     })
+
+    app.refresh()
 })
--- src/server/app.js
+++ src/server/app.js
@@ -39,6 +39,18 @@ app.get('/browser', async (req, res) => {
     res.end(ua.isChrome ? "browser is supported!" : JSON.stringify(ua));
 })
 
+const Cards = {
+    calls : [],
+    responses : []
+}
+
+function getCall() {
+    return Cards.calls[Math.floor(Math.random() * Cards.calls.length)]
+}
+function getResponse() {
+    return Cards.responses[Math.floor(Math.random() * Cards.responses.length)]
+}
+
 const Game = {
     users : [],
     cardCzar : 0,
@@ -46,8 +58,6 @@ const Game = {
     playedCards : [],
     currentCard : undefined,
     inProgress : false,
-    calls : [],
-    responses : []
 }
 
 app.post('/login', async (req, res) => {
@@ -55,27 +65,27 @@ app.post('/login', async (req, res) => {
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end("bad");
     }
-    Game.users.push({ 
+    const user = { 
         nickname : req.body.nickname,
         ready : false,
         choseCard : false,
         score : 0,
-    })
+        cards : []
+    }
+    for (let step = 0; step < 7; step++) {
+        user.cards.push(getResponse())
+    }
+    Game.users.push(user)
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end("ok");
 })
 
-app.get('/card', async (req, res) => {
-    const rnd = Math.floor(Math.random() * Game.responses.length)
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(Game.responses[rnd].text[0]);
-})
-
 app.post('/card', async (req, res) => {
     user = Game.users.find(user => user.nickname == req.body.nickname)
+    card = user.cards.find(card => card.id == req.body.id)
     playedCards.push({
-        text : req.body.text,
-        user : user.nickname
+        card : card,
+        user : req.body.nickname
     })
     user.choseCard = true
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -83,14 +93,15 @@ app.post('/card', async (req, res) => {
 })
 
 app.post('/winner', async (req, res) => {
-    user = Game.users.find(user => user.nickname == req.body.nickname)
+    winner = Game.playedCards.find(play => play.card.id == req.body.id)
+    user = Game.users.find(user => user.nickname == winner.user)
     user.score += 1
-    cardCzar = (cardCzar + 1) % Game.users.length
+    Game.cardCzar = (Game.cardCzar + 1) % Game.users.length
     for(user of Game.users) {
         user.choseCard = false
     }
     Game.playedCards = []
-    Game.currentCard = Game.calls[Math.floor(Math.random() * Game.calls.length)].text[0]
+    Game.currentCard = getCall()
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end("ok");
 })
@@ -100,7 +111,7 @@ app.post('/ready', async (req, res) => {
     user.ready = true
     Game.numReady += 1
     if (Game.numReady > 1 && Game.numReady == Game.users.length) {
-        Game.currentCard = Game.calls[Math.floor(Math.random() * Game.calls.length)].text[0]
+        Game.currentCard = getCall()
         Game.inProgress = true
     }
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -114,19 +125,29 @@ app.get('/users', async (req, res) => {
 
 app.post('/pack/:id', async (req, res) => {
     try {
-        let newCalls = await fetch(`https://api.cardcastgame.com/v1/decks/${req.params.id}/calls`)
-        let newResponses = await fetch(`https://api.cardcastgame.com/v1/decks/${req.params.id}/responses`)
-        Game.calls = Game.calls.concat(await newCalls.json())
-        Game.responses = Game.responses.concat(await newResponses.json())
+        const newCalls = await fetch(`https://api.cardcastgame.com/v1/decks/${req.params.id}/calls`)
+        const newResponses = await fetch(`https://api.cardcastgame.com/v1/decks/${req.params.id}/responses`)
+        Cards.calls = Cards.calls.concat(await newCalls.json())
+        Cards.responses = Cards.responses.concat(await newResponses.json())
     }
     catch(error) {
         console.log(error)
         res.writeHead(500, {'Content-Type': 'text/plain'});
         res.end("error");
+        return
     }
     
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end("ok");
 })
 
-app.listen(80, () => console.log(`listening on port 80!`))
+
+app.listen(80, async () => {
+    // testing, but maybe do this always with some default pack?
+    const newCalls = await fetch(`https://api.cardcastgame.com/v1/decks/6QP6Y/calls`)
+    const newResponses = await fetch(`https://api.cardcastgame.com/v1/decks/6QP6Y/responses`)
+    Cards.calls = Cards.calls.concat(await newCalls.json())
+    Cards.responses = Cards.responses.concat(await newResponses.json())
+    console.log(Cards)
+    console.log(`listening on port 80!`)
+})
